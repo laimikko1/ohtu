a22e4b2119fac8aafb5f72b6dc45693a9ba44a79
laimikko1 <mikko.s.laitinen@helsinki.fi>
2017-11-07 08:28:07 +0200

    Spring hallinnoi nyt projektia

--- Verkkokauppa1/src/main/java/ohtu/Main.java
+++ Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -2,12 +2,20 @@ package ohtu;
 
 import ohtu.verkkokauppa.Kauppa;
 import ohtu.verkkokauppa.Kirjanpito;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
-        Kauppa kauppa = new Kauppa();
-
+//        Kirjanpito kirjanpito = new Kirjanpito();
+//        Varasto varasto = new Varasto(kirjanpito);
+//        Pankki pankki = new Pankki(kirjanpito);
+//        Viitegeneraattori viitegen = new Viitegeneraattori();
+//        Kauppa kauppa = new Kauppa(varasto, pankki, viitegen);
+        ApplicationContext ctx = new FileSystemXmlApplicationContext("src/main/resources/spring-context.xml");
+        Kauppa kauppa = (Kauppa) ctx.getBean("kauppa");
+        Kirjanpito kirjanpito = (Kirjanpito) ctx.getBean("kirjanpito");
         // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
         kauppa.aloitaAsiointi();
         kauppa.lisaaKoriin(1);
@@ -25,7 +33,7 @@ public class Main {
         kauppa.tilimaksu("Arto Vihavainen", "3425-1652");
 
         // kirjanpito
-        for (String tapahtuma : Kirjanpito.getInstance().getTapahtumat()) {
+        for (String tapahtuma : kirjanpito.getTapahtumat()) {
             System.out.println(tapahtuma);
         }
     }
